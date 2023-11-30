9d77137547d22dd2f181b49f8576d4d2831a8da1
HWQ <gitlab@goat.com>
2023-11-30 14:07:52 +0800

    Initial commit

---
+++ src/test/java/com/hwq/goatapigateway/GoatapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwq.goatapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
